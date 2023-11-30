ef1cf4b3be28b73625f64daddd8e58eebb573858
MJ512 <96252493+MJ512@users.noreply.github.com>
2023-12-01 01:27:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ yesBtn.addEventListener("click", () => {
   question.innerHTML = "I love You My Angel";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+  const audio = document.querySelector(".audio-player");
+  audio.play();
   yesBtn.style.display = "none";
   noBtn.style.display = "none";
 });
